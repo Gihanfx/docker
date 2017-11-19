cfb5f2698f071e7a69a68854ef050571b7d9bd61
Gihan <gihanldes@gmail.com>
2017-11-19 22:43:22 +0530

    Add print for console check

--- RestBackend/src/main/java/com/gim/restapi/RestBackend/MyResource.java
+++ RestBackend/src/main/java/com/gim/restapi/RestBackend/MyResource.java
@@ -1,5 +1,7 @@
 package com.gim.restapi.RestBackend;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,6 +14,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
+    	System.out.println("Test console");
         return "Hi from backend from Docker V5";
     }
 }
